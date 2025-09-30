ffd3325ecad6c64f76d96eb380a04172bff4161a
NicoleLima0 <nick01tererj@gmail.com>
2025-09-30 20:36:39 -0300

    feat: Ajuste build

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-import fs from "fs";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   base: "/album-web/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
-  buildEnd() {
-    const indexPath = resolve(__dirname, "dist/index.html");
-    const notFoundPath = resolve(__dirname, "dist/404.html");
-    if (fs.existsSync(indexPath)) {
-      fs.copyFileSync(indexPath, notFoundPath);
-    }
-  },
 });
